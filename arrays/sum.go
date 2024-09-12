737be5ab8ab2b8ee7deaeb5eb2ce225fe29e7b7d
Martin Barber <martinpbarber@gmail.com>
2024-09-12 14:02:12 -0400

    Slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,6 +1,7 @@
 package main
 
-func Sum(numbers [5]int) int {
+// Sum calculates the total from an array of numbers.
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
