d27254ffef9aa11ece0ceed92bd2c11e36d13c26
Martin Barber <martinpbarber@gmail.com>
2024-09-14 09:38:48 -0400

    Rectangle

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,11 +1,16 @@
 package shapes
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter returns the perimeter of a rectangle.
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 // Area returns the area of a rectangle
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
 }
