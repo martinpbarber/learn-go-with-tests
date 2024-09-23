001f8b18768fa791d36af38dbbb6c865f26dcee6
Martin Barber <martinpbarber@gmail.com>
2024-09-23 15:11:55 -0400

    Word exists

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,6 +3,7 @@ package dictionary
 import "errors"
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("cannot add word because it already exists")
 
 // Dictionary stores words and their definitions.
 type Dictionary map[string]string
@@ -18,6 +19,17 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // Add puts a word and its definition in the dictionary.
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
