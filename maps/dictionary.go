f75b1658e25e0342d6318132cda569b38375e4e8
Martin Barber <martinpbarber@gmail.com>
2024-09-23 13:55:18 -0400

    Unknown word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,9 +1,16 @@
 package dictionary
 
+import "errors"
+
 // Dictionary stores words and their definitions.
 type Dictionary map[string]string
 
 // Seach returns the definition for a given word.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
