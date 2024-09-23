158d94c1f933f915339409635a7e8acaa0f283b3
Martin Barber <martinpbarber@gmail.com>
2024-09-23 14:03:03 -0400

    Refactor

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,6 +2,8 @@ package dictionary
 
 import "errors"
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Dictionary stores words and their definitions.
 type Dictionary map[string]string
 
@@ -9,7 +11,7 @@ type Dictionary map[string]string
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
