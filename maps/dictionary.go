67564789a45e76b71295e83e81b519badb8b5290
Martin Barber <martinpbarber@gmail.com>
2024-09-23 13:42:49 -0400

    Add docs

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,9 @@
 package dictionary
 
+// Dictionary stores words and their definitions.
 type Dictionary map[string]string
 
+// Seach returns the definition for a given word.
 func (d Dictionary) Search(word string) string {
 	return d[word]
 }
